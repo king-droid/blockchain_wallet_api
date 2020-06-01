e3ca9f86fa322ca715d5bdbdd135be76b57fce73
Naresh Yadav <yadav.naresh07@gmail.com>
2020-06-01 22:41:11 +0530

    package.json update

--- scripts/start.js
+++ scripts/start.js
@@ -1,10 +1,6 @@
 'use strict';
 
-try {
-  require('node-env-file')('.env');
-} catch (e) {
-  console.log(e);
-} finally {
+
   var merchantAPI = require('../index.js');
   merchantAPI.start({
     port: process.env.PORT || 5000,
@@ -15,4 +11,4 @@ try {
       api_code: process.env.API_CODE
     });
   }
-}
+
